3e54e87b2fd1e84ec15de2e4e0afd50c458921e0
Dixita <dixitabhanderi101@gmail.com>
2023-08-25 21:40:32 -0700

    react front end blog

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,26 @@
+// localhost:3000/articles/learn-node
+import { parsePath, useParams } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
+import articles from "./article-content";
+
+const ArticlePage = () => {
+    // const params = useParams();
+    // const articleId  = params.articleId;
+    // const articleId  = params;
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+    if(!article) {
+        return <NotFoundPage />
+    } 
+    
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+        </>
+    );
+}
+
+export default ArticlePage;
---
+++ src/pages/NavBar.js
@@ -0,0 +1,21 @@
+import { Link, link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/articles">Articles</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
